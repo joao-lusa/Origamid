ff6950d93b21a696fd3214d702aa1eea061d4f1e
joao-lusa <joaosombrio@rede.ulbra.br>
2022-05-10 11:27:37 -0300

    estilizando o carrinho de compras

--- VueCompleto/04-techno-projeto/13estilo-carrinho/techno/app.js
+++ VueCompleto/04-techno-projeto/13estilo-carrinho/techno/app.js
@@ -4,6 +4,7 @@ const vm = new Vue({
         produtos: [],
         produto: false,
         carrinho: [],
+        carrinhoAtivo: false,
         mensagemAlerta: "Item adicionado",
         alertaAtivo: false
     },
@@ -47,6 +48,9 @@ const vm = new Vue({
         },
         fecharModal({target, currentTarget}){
             if(target === currentTarget) this.produto = false
+        },
+        clickForaCarrinho({target, currentTarget}){
+            if(target === currentTarget) this.carrinhoAtivo = false
         },
         adicionarItem(){
             this.produto.estoque--;
