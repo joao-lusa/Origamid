a8cb9d84bb0ca0c95c323af2bf08f1d3a2d72d80
joao-lusa <joaosombrio@rede.ulbra.br>
2022-05-12 15:57:07 -0300

    aula sobre a estrutura

---
+++ VueCompleto/05-componentes/05vue-estrutura/app-vue/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div id="app">{{ mensagem }}</div>
+    <HeaderPrincipal />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      mensagem: "Essa é uma mensagem",
+    };
+  },
+  components: {},
+};
+</script>
+
+<style>
+</style>
