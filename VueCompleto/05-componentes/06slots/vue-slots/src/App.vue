2ca976df462479b9f1fccef29d401d53721dc92b
joao-lusa <joaosombrio@rede.ulbra.br>
2022-05-12 18:39:09 -0300

    aprendendo sobre slots do vue

---
+++ VueCompleto/05-componentes/06slots/vue-slots/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <ModalPrincipal>
+    <template #header="{ logo }">
+      {{ logo }}
+      Esse é o header
+    </template>
+    <template v-slot:default="{ user }">
+      {{ user.nome }}
+      <p>este é um teste</p>
+      <button>botão</button>
+    </template>
+    <template #footer> <p>esse é o footer</p> </template>
+  </ModalPrincipal>
+</template>
+
+<script>
+import ModalPrincipal from "./components/ModalPrincipal.vue";
+export default {
+  name: "App",
+  components: {
+    ModalPrincipal,
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ VueCompleto/05-componentes/06slots/vue-slots/src/components/ModalPrincipal.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="modal">
+    <slot name="header" :logo="logo"></slot>
+    <h1>{{ user.nome }}</h1>
+    <slot :user="user"></slot>
+    <slot name="footer"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ModalPrincipal",
+  data() {
+    return {
+      logo: "techno",
+      user: {
+        nome: "Andre",
+      },
+    };
+  },
+};
+</script>
+
+<style>
+</style>
