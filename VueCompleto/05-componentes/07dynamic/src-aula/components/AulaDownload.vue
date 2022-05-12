0c4b04d6b19325ae4d9aa793c4c694d1c42db302
joao-lusa <joaosombrio@rede.ulbra.br>
2022-05-12 20:00:52 -0300

    aula de dynamic

---
+++ VueCompleto/05-componentes/07dynamic/src-aula/components/AulaDownload.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <p>Faça o download dos arquivos.</p>
+    <button>Download</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AulaDownload",
+  destroyed() {
+    console.log("download destruido");
+  },
+  created() {
+    console.log("download criado");
+  },
+};
+</script>
+
+<style>
+</style>
