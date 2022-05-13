2454119619cc354717c1ba3f2cc82e46a5b3cee7
joao-lusa <joaosombrio@rede.ulbra.br>
2022-05-13 12:01:05 -0300

    Animações com vue

---
+++ VueCompleto/06-animacoes/App-aula.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <button @click="ativo = !ativo">servicos</button>
+    <transition appear name="fade" @enter="enter" @leave="leave">
+      <h1>Serviço</h1>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      ativo: false,
+    };
+  },
+  methods: {
+    enter(elemento) {
+      console.log(elemento.classList.add("teste"));
+    },
+    leave() {
+      console.log("sumiu");
+    },
+  },
+  components: {},
+};
+</script>
+
+<style>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 2s;
+}
+
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
