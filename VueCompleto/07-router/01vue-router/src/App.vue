728a6ed38bf74e4c6b6d9198ee8b388d1516496d
joao-lusa <joaosombrio@rede.ulbra.br>
2022-05-16 11:33:58 -0300

    Router

---
+++ VueCompleto/07-router/01vue-router/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <h1 @click="mostrarRouter">logo</h1>
+    <nav>
+      <router-link to="/">Home</router-link>
+      <router-link to="/cursos">Cursos</router-link>
+    </nav>
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    mostrarRouter() {
+      console.log(this.$route);
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+a {
+  padding: 10px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #84e;
+}
+</style>
---
+++ VueCompleto/07-router/01vue-router/src/views/Cursos.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="home">
+    <h1>Essa é a cursos</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeView",
+  components: {},
+};
+</script>
