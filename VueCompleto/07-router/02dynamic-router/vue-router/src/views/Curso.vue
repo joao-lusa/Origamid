01a6c8772090b0eb9be5e31850ed494917138093
joao-lusa <joaosombrio@rede.ulbra.br>
2022-05-16 14:20:33 -0300

    children routers

---
+++ VueCompleto/07-router/02dynamic-router/vue-router/src/views/Curso.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <h1>{{ curso }}</h1>
+    <router-link :to="{ name: 'descricao' }">Descrição</router-link>
+    <router-link :to="{ name: 'aulas' }">Aulas</router-link>
+
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CursoView",
+  props: ["curso"],
+  created() {
+    console.log("compoente criado");
+  },
+  beforeRouteUpdate(to, from, next) {
+    next();
+  },
+};
+</script>
---
+++ VueCompleto/07-router/02dynamic-router/vue-router/src/views/Cursos.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    <h1>Curso Ativo: {{ curso }}</h1>
+    <router-link :to="{ name: 'curso', params: { curso: 'javascript' } }"
+      >JavaScript</router-link
+    >
+    <router-link to="/cursos/html">HTML</router-link>
+    <router-link to="/cursos/css">CSS</router-link>
+
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CursosView",
+  props: ["curso"],
+  created() {
+    console.log("compoente criado");
+  },
+  beforeRouteUpdate(to, from, next) {
+    next();
+  },
+};
+</script>
