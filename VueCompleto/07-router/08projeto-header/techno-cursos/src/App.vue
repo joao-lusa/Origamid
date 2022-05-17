192f23f1e377d127916cf9def60f3f6e4a130636
joao-lusa <joaosombrio@rede.ulbra.br>
2022-05-17 16:39:46 -0300

    header do projeto feito

---
+++ VueCompleto/07-router/08projeto-header/techno-cursos/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <TheHeader></TheHeader>
+    <router-view></router-view>
+  </div>
+</template>
+<script>
+import TheHeader from "@/components/TheHeader.vue";
+export default {
+  components: {
+    TheHeader,
+  },
+};
+</script>
+<style>
+body {
+  margin: 0px;
+  font-family: Arial, Helvetica, sans-serif;
+  color: #234;
+}
+ul {
+  list-style: none;
+  padding: 0px;
+  margin: 0px;
+}
+a {
+  font-weight: bold;
+  text-decoration: none;
+  color: #234;
+}
+a.router-link-active {
+  color: #4b8;
+}
+</style>
+
