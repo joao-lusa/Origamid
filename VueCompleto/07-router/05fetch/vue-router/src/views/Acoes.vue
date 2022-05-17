9ecdebec2b97a3256a6505e25f044605b2a71727
joao-lusa <joaosombrio@rede.ulbra.br>
2022-05-17 11:49:43 -0300

    Usando fetch com routers

---
+++ VueCompleto/07-router/05fetch/vue-router/src/views/Acoes.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h2>Ações</h2>
+    <router-link to="/acoes/aapl">Apple</router-link>
+    <router-link to="/acoes/fb">FaceBoock</router-link>
+
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AcoesViews",
+};
+</script>
+
+<style>
+</style>
