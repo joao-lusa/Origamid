c36a092c1adaa957b8e1aeb3f15e85e4284749b0
joao-lusa <joaosombrio@rede.ulbra.br>
2022-05-17 18:56:28 -0300

    puxando api no mixins

---
+++ VueCompleto/07-router/09mixins/techno-cursos/src/views/Cursos.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div v-if="loading">
+      <p>carrengando....</p>
+    </div>
+    <div v-if="api">
+      <h1>Cursos</h1>
+      <p>{{ api }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import fetchData from "@/mixins/fetchData.js";
+
+export default {
+  name: "CursosView",
+  mixins: [fetchData],
+  created() {
+    this.fetchData("/cursos");
+  },
+};
+</script>
