ed888aa7ea182779ea327a8a0a1b0e90ca2585d9
joao-lusa <joaosombrio@rede.ulbra.br>
2022-05-18 11:06:15 -0300

    pagina home estilizada

---
+++ VueCompleto/07-router/11home/techno-cursos/src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <div id="app">
+    <TheHeader></TheHeader>
+    <router-view class="container"></router-view>
+  </div>
+</template>
+<script>
+import TheHeader from "@/components/TheHeader.vue";
+export default {
+  components: {
+    TheHeader,
+  },
+};
+</script>
+
+<style>
+body {
+  margin: 0px;
+  font-family: Arial, Helvetica, sans-serif;
+  color: #234;
+}
+ul {
+  list-style: none;
+  padding: 0px;
+  margin: 0px;
+}
+a {
+  font-weight: bold;
+  text-decoration: none;
+  color: #234;
+}
+a.router-link-active {
+  color: #4b8;
+}
+img {
+  max-width: 100%;
+}
+.container {
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 20px;
+}
+
+.v-enter {
+  opacity: 0;
+  transform: translate3d(-20px, 0, 0);
+}
+
+.v-enter-active {
+  transition: all 0.3s;
+}
+
+.conteudo {
+  display: grid;
+  grid-template-columns: 1fr minmax(250px, 450px);
+  grid-gap: 30px;
+}
+@media screen and (max-width: 550px) {
+  .conteudo {
+    display: block;
+  }
+}
+</style>
+
---
+++ VueCompleto/07-router/11home/techno-cursos/src/views/Cursos.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div v-if="loading">
+      <PageLoading></PageLoading>
+    </div>
+    <transition>
+      <div v-if="api">
+        <h1>Cursos</h1>
+        <p>{{ api }}</p>
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script>
+import fetchData from "@/mixins/fetchData.js";
+
+export default {
+  name: "CursosView",
+  mixins: [fetchData],
+  created() {
+    this.fetchData("/cursos");
+  },
+};
+</script>
