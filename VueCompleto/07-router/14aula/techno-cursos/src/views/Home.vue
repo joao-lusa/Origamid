ad7b2aef7404c08b5c13a36c6250c3d48644b8d0
joao-lusa <joaosombrio@rede.ulbra.br>
2022-05-18 14:30:23 -0300

    projeto feito

---
+++ VueCompleto/07-router/14aula/techno-cursos/src/views/Home.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <div v-if="loading">
+      <PageLoading></PageLoading>
+    </div>
+    <transition>
+      <div v-if="api" class="conteudo">
+        <div>
+          <h1>Sobre a {{ api.titulo }}</h1>
+          <p>{{ api.descricao }}</p>
+          <router-link class="btn-cursos" to="/cursos" tag="button"
+            >Cursos</router-link
+          >
+          <div>
+            <h2>Avaliações</h2>
+            <ul>
+              <li v-for="avaliacao in api.avaliacoes" :key="avaliacao.nome">
+                <p>{{ avaliacao.nome }}</p>
+                <p>{{ avaliacao.descricao }}</p>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <img src="../assets/aprender.png" alt="Aprenda Web Design" />
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script>
+import fetchData from "@/mixins/fetchData.js";
+import PageLoading from "@/components/PageLoading.vue";
+
+export default {
+  name: "HomeView",
+  mixins: [fetchData],
+  created() {
+    this.fetchData("/home");
+  },
+  components: { PageLoading },
+};
+</script>
+
+<style>
+.btn-cursos {
+  border: none;
+  background: #4b8;
+  border-radius: 4px;
+  color: white;
+  cursor: pointer;
+  padding: 15px 25px;
+  font-size: 1rem;
+  margin-top: 10px;
+  margin-bottom: 40px;
+  box-shadow: 0 4px 2px rgba(0, 0, 0, 0.1);
+  font-family: Arial, Helvetica, sans-serif;
+}
+</style>
