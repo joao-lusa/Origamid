ed305fe5517d48ac91a83b96f83a870c62a48e39
joao-lusa <joaosombrio@rede.ulbra.br>
2022-05-18 16:27:48 -0300

    começando Vuex

---
+++ VueCompleto/08-Vuex/01state/app-vuex/src/components/Aula.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <p>
+      Aula {{ totalAula }}, esse é o usuario {{ user }}, {{ aulasCompletas }}
+    </p>
+    <button @click="mostrarConsole">Mostrar no console</button>
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+export default {
+  name: "AulaComponente",
+  data() {
+    return {
+      totalAula: 25,
+      nome: "João Lucas",
+    };
+  },
+  computed: {
+    ...mapState(["user", "aulasCompletas"]),
+    nomeMaiusculo() {
+      return this.nome.toUpperCase();
+    },
+  },
+  methods: {
+    mostrarConsole() {
+      console.log(this.user);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ VueCompleto/08-Vuex/01state/app-vuex/src/components/User.vue
@@ -0,0 +1,17 @@
+<template>
+  <p>User {{ aulasCompletas }}</p>
+</template>
+
+<script>
+export default {
+  name: "AulaComponente",
+  computed: {
+    aulasCompletas() {
+      return this.$store.state.aulasCompletas;
+    },
+  },
+};
+</script>
+
+<style>
+</style>
