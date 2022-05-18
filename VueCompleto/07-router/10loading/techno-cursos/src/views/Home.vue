8618a0901d3bedf2a2e3685a2d144a962a7497a6
joao-lusa <joaosombrio@rede.ulbra.br>
2022-05-18 10:16:13 -0300

    animando o loading

---
+++ VueCompleto/07-router/10loading/techno-cursos/src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div v-if="loading">
+      <PageLoading></PageLoading>
+    </div>
+    <div v-if="api">
+      <h1>Home</h1>
+      <p>{{ api }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import fetchData from "@/mixins/fetchData.js";
+import PageLoading from "@/components/PageLoading.vue";
+
+export default {
+  name: "HomeView",
+  mixins: [fetchData],
+  created() {
+    this.fetchData("/home");
+  },
+  components: { PageLoading },
+};
+</script>
