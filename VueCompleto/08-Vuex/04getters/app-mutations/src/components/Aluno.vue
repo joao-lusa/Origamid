d4fb8b1768cb3abeee088b52b7af2b68de2407af
joao-lusa <joaosombrio@rede.ulbra.br>
2022-05-20 10:47:12 -0300

    getters

---
+++ VueCompleto/08-Vuex/04getters/app-mutations/src/components/Aluno.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <p>Aluno {{ aulasCompletas }}</p>
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+
+export default {
+  name: "AlunoComponent",
+  computed: {
+    ...mapState(["aulasCompletas"]),
+  },
+};
+</script>
+
+<style>
+</style>
