d7a09c9af0d2f8112e5a37178f2b4efd98ff3ef5
joao-lusa <joaosombrio@rede.ulbra.br>
2022-05-20 11:12:15 -0300

    Modules do vuex

---
+++ VueCompleto/08-Vuex/05modules/app-mutations/src/store/index.js
@@ -0,0 +1,43 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import acao from '@/store/acao'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  modules: {
+    acao
+  },
+  state: {
+    aulasCompletas: [],
+    acao:null,
+    livros: [
+      {
+        nome: "O Senhor dos Anéis",
+        lido: true,
+      },
+      {
+        nome: "Harry Potter",
+        lido: true,
+      },
+      {
+        nome: "As Crônicas de Gelo e Fogo",
+        lido: false,
+      }
+    ]
+  },
+  getters: {
+    livrosLidos: state => lido => state.livros.filter(livro => livro.lido === lido)
+  },
+  mutations: {
+    COMPLETAR_AULA(state, payload){
+      state.aulasCompletas.push(payload)
+    },
+  
+  },
+  actions: {
+    completarAula(context, payload){
+      context.commit("COMPLETAR_AULA", payload)
+    }
+  }
+})
