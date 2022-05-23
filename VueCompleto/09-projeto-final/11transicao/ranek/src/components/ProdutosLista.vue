81de2eb7b6e8b509fdd33362468cc9ac0adf2144
joao-lusa <joaosombrio@rede.ulbra.br>
2022-05-23 16:14:17 -0300

    animando paginação

--- VueCompleto/09-projeto-final/11transicao/ranek/src/components/ProdutosLista.vue
+++ VueCompleto/09-projeto-final/11transicao/ranek/src/components/ProdutosLista.vue
@@ -1,28 +1,31 @@
 <template>
   <section class="produtos-container">
-    <div v-if="produtos && produtos.length" class="produtos">
-      <div class="produto" v-for="(produto, index) in produtos" :key="index">
-        <router-link to="/">
-          <img
-            v-if="produto.fotos"
-            :src="produto.fotos[0].src"
-            :alt="produto.fotos[0].titulo"
-          />
-          <p class="preco">{{ produto.preco }}</p>
-          <h2 class="titulo">{{ produto.nome }}</h2>
-          <p>{{ produto.descricao }}</p>
-        </router-link>
+    <transition mode="out-in">
+      <div v-if="produtos && produtos.length" class="produtos" key="produtos">
+        <div class="produto" v-for="(produto, index) in produtos" :key="index">
+          <router-link to="/">
+            <img
+              v-if="produto.fotos"
+              :src="produto.fotos[0].src"
+              :alt="produto.fotos[0].titulo"
+            />
+            <p class="preco">{{ produto.preco }}</p>
+            <h2 class="titulo">{{ produto.nome }}</h2>
+            <p>{{ produto.descricao }}</p>
+          </router-link>
+        </div>
+        <ProdutosPaginar
+          :produtosTotal="produtosTotal"
+          :produtosPorPagina="produtosPorPagina"
+        />
       </div>
-      <ProdutosPaginar
-        :produtosTotal="produtosTotal"
-        :produtosPorPagina="produtosPorPagina"
-      />
-    </div>
-    <div v-else-if="produtos && produtos.length === 0">
-      <p class="sem-resultados">
-        Busca sem resultados. Tente buscar outro termo.
-      </p>
-    </div>
+      <div v-else-if="produtos && produtos.length === 0" key="sem resultados">
+        <p class="sem-resultados">
+          Busca sem resultados. Tente buscar outro termo.
+        </p>
+      </div>
+      <PaginaCarregando v-else key="carregando" />
+    </transition>
   </section>
 </template>
 
@@ -30,16 +33,18 @@
 import ProdutosPaginar from "@/components/ProdutosPaginar.vue";
 import { api } from "@/services.js";
 import { serialize } from "@/helpers.js";
+import PaginaCarregando from "./PaginaCarregando.vue";
 
 export default {
   name: "ProdutosLista",
   components: {
     ProdutosPaginar,
+    PaginaCarregando,
   },
   data() {
     return {
       produtos: null,
-      produtosPorPagina: 3,
+      produtosPorPagina: 9,
       produtosTotal: 0,
     };
   },
@@ -51,10 +56,13 @@ export default {
   },
   methods: {
     getProdutos() {
-      api.get(this.url).then((response) => {
-        this.produtosTotal = Number(response.headers["x-total-count"]);
-        this.produtos = response.data;
-      });
+      this.produtos = null;
+      setTimeout(() => {
+        api.get(this.url).then((response) => {
+          this.produtosTotal = Number(response.headers["x-total-count"]);
+          this.produtos = response.data;
+        });
+      }, 1500);
     },
   },
   watch: {
