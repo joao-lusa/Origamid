ab9eb901e89a07ec92e86b7228af6db7b73d818f
joao-lusa <joaosombrio@rede.ulbra.br>
2022-05-23 22:31:15 -0300

    Mutations com vuex para o login

---
+++ VueCompleto/09-projeto-final/14vuex-login/ranek/src/router/index.js
@@ -0,0 +1,39 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import Produto from '../views/Produto.vue'
+import Login from '../views/Login.vue'
+import Usuario from '../views/usuario/Usuario.vue'
+
+Vue.use(VueRouter)
+
+export default new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: "/",
+      name:"home",
+      component:HomeView
+    },
+    {
+      path: "/produto/:id",
+      name:"produto",
+      component:Produto,
+      props: true
+    },
+    {
+      path: "/login",
+      name:"login",
+      component: Login
+    },
+    {
+      path: "/usuario",
+      name:"usuario",
+      component: Usuario
+    }
+  ],
+  scrollBehavior(){
+    return window.scrollTo({top:0, behavior: "smooth"})
+  }
+})
---
+++ VueCompleto/09-projeto-final/14vuex-login/ranek/src/views/HomeView.vue
@@ -0,0 +1,28 @@
+<template>
+  <section class="home">
+    <h1>👍Compre ou venda👎</h1>
+    <ProdutosBuscar></ProdutosBuscar>
+    <ProdutosLista></ProdutosLista>
+  </section>
+</template>
+
+<script>
+import ProdutosBuscar from "@/components/ProdutosBuscar.vue";
+import ProdutosLista from "@/components/ProdutosLista.vue";
+export default {
+  name: "HomeView",
+  components: {
+    ProdutosBuscar,
+    ProdutosLista,
+  },
+};
+</script>
+
+<style scoped>
+h1 {
+  color: #87f;
+  margin-top: 40px;
+  font-size: 2rem;
+  text-align: center;
+}
+</style>
