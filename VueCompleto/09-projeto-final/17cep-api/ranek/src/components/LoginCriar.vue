7ea63936d1d6a7e67bf727139a6ce23bca63b892
joao-lusa <joaosombrio@rede.ulbra.br>
2022-05-24 14:05:54 -0300

    Pegando informações do usuário pelo cep

---
+++ VueCompleto/09-projeto-final/17cep-api/ranek/src/components/LoginCriar.vue
@@ -0,0 +1,45 @@
+<template>
+  <section>
+    <h2>Crie a sua conta</h2>
+    <transition mode="out-in">
+      <button v-if="!criar" class="btn" @click="criar = true">
+        Criar conta
+      </button>
+      <UsuarioForm v-else>
+        <button class="btn btn-form">Criar usuario</button>
+      </UsuarioForm>
+    </transition>
+  </section>
+</template>
+
+<script>
+import UsuarioForm from "@/components/UsuarioForm.vue";
+export default {
+  name: "LoginCriar",
+  data() {
+    return {
+      criar: false,
+    };
+  },
+  components: {
+    UsuarioForm,
+  },
+};
+</script>
+
+<style scoped>
+h2 {
+  text-align: center;
+  margin-top: 40px;
+  margin-bottom: 10px;
+}
+.btn {
+  width: 100%;
+  max-width: 300px;
+  margin-left: auto;
+  margin-right: auto;
+}
+.btn-form {
+  max-width: 100%;
+}
+</style>
---
+++ VueCompleto/09-projeto-final/17cep-api/ranek/src/store/index.js
@@ -0,0 +1,46 @@
+import { api } from '@/services'
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  strict: true,
+  state: {
+    login: false,
+    usuario:{
+      id: "",
+      nome: "",
+      email: "",
+      senha: "",
+      cep: "",
+      rua: "",
+      numero: "",
+      bairro: "",
+      cidade: "",
+      estado: "",
+
+    }
+  },
+  getters: {
+  },
+  mutations: {
+    UPDATE_LOGIN(state, payload){
+      state.login = payload
+    },
+    UPDATE_USUARIO(state, payload){
+      state.usuario = Object.assign(state.usuario, payload)
+    }
+  },
+  actions: {
+    getUsuario(context, payload){
+      api.get(`/usuario/${payload}`).then(r=>{
+        context.commit("UPDATE_USUARIO", r.data);
+        context.commit("UPDATE_LOGIN", true)
+      })
+    }
+  },
+  modules: {
+  }
+})
