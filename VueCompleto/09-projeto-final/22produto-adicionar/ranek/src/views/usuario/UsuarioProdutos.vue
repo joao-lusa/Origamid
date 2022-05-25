40a1c0d29cf85c324dae58e9648f45dedfabd1e9
joao-lusa <joaosombrio@rede.ulbra.br>
2022-05-25 14:12:55 -0300

    Adicionar produtos

---
+++ VueCompleto/09-projeto-final/22produto-adicionar/ranek/src/views/usuario/UsuarioProdutos.vue
@@ -0,0 +1,60 @@
+<template>
+  <section>
+    <h2>Adicionar produto</h2>
+    <ProdudoAdicionar></ProdudoAdicionar>
+    <h2>Seus Produtos</h2>
+    <transition-group v-if="usuario_produtos" name="list" tag="ul">
+      <li v-for="(produto, index) in usuario_produtos" :key="index">
+        <ProdutoItem :produto="produto">
+          <p>{{ produto.descricao }}</p>
+        </ProdutoItem>
+      </li>
+    </transition-group>
+  </section>
+</template>
+
+<script>
+import ProdudoAdicionar from "@/components/ProdudoAdicionar.vue";
+import { mapState, mapActions } from "vuex";
+import ProdutoItem from "../../components/ProdutoItem.vue";
+
+export default {
+  name: "UsuarioProduto",
+  components: {
+    ProdudoAdicionar,
+    ProdutoItem,
+  },
+  computed: {
+    ...mapState(["login", "usuario", "usuario_produtos"]),
+  },
+  methods: {
+    ...mapActions(["getUsuarioProdutos"]),
+  },
+  watch: {
+    login() {
+      this.getUsuarioProdutos();
+    },
+  },
+  created() {
+    if (this.login) {
+      this.getUsuarioProdutos();
+    }
+  },
+};
+</script>
+
+<style scoped>
+h2 {
+  margin-bottom: 20px;
+}
+.list-enter,
+.list-leave-to {
+  opacity: 0;
+  transform: translate3d(20px, 0, 0);
+}
+
+.list-enter-active,
+.list-leave-actve {
+  transition: all 0.3s;
+}
+</style>
