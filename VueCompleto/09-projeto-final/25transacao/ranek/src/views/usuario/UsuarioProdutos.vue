e9f58c494a828001860ac7a05fd52c6fc823c363
joao-lusa <joaosombrio@rede.ulbra.br>
2022-05-25 16:55:47 -0300

    Usuário atualizar

---
+++ VueCompleto/09-projeto-final/25transacao/ranek/src/views/usuario/UsuarioProdutos.vue
@@ -0,0 +1,88 @@
+<template>
+  <section>
+    <h2>Adicionar produto</h2>
+    <ProdudoAdicionar></ProdudoAdicionar>
+    <h2>Seus Produtos</h2>
+    <transition-group v-if="usuario_produtos" name="list" tag="ul">
+      <li v-for="(produto, index) in usuario_produtos" :key="index">
+        <ProdutoItem :produto="produto">
+          <p>{{ produto.descricao }}</p>
+          <button class="deletar" @click="deletarProduto(produto.id)"></button>
+        </ProdutoItem>
+      </li>
+    </transition-group>
+  </section>
+</template>
+
+<script>
+import ProdudoAdicionar from "@/components/ProdudoAdicionar.vue";
+import { mapState, mapActions } from "vuex";
+import ProdutoItem from "../../components/ProdutoItem.vue";
+import { api } from "@/services.js";
+
+export default {
+  name: "UsuarioProduto",
+  components: {
+    ProdudoAdicionar,
+    ProdutoItem,
+  },
+  computed: {
+    ...mapState(["login", "usuario", "usuario_produtos"]),
+  },
+  methods: {
+    ...mapActions(["getUsuarioProdutos"]),
+    deletarProduto(id) {
+      const confirmar = window.confirm("Deseja remover este produto?");
+      if (confirmar) {
+        api
+          .delete(`/produto/${id}`)
+          .then(() => {
+            this.getUsuarioProdutos();
+          })
+          .catch((error) => {
+            console.log(error.r);
+          });
+      }
+    },
+  },
+  watch: {
+    login() {
+      this.getUsuarioProdutos();
+    },
+  },
+  created() {
+    if (this.login) {
+      this.getUsuarioProdutos();
+    }
+  },
+};
+</script>
+
+<style scoped>
+h2 {
+  margin-bottom: 20px;
+}
+.list-enter,
+.list-leave-to {
+  opacity: 0;
+  transform: translate3d(20px, 0, 0);
+}
+
+.list-enter-active,
+.list-leave-actve {
+  transition: all 0.3s;
+}
+
+.deletar {
+  position: absolute;
+  top: 0px;
+  right: 0px;
+  background: url("../../assets/remove.svg") no-repeat center center;
+  width: 24px;
+  height: 24px;
+  text-indent: -140px;
+  overflow: hidden;
+  cursor: pointer;
+  border: none;
+}
+</style>
