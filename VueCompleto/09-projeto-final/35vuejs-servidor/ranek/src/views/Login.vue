f2cfa0a046ce71a74f98d4ad038f6863a3e9e4c0
joao-lusa <joaosombrio@rede.ulbra.br>
2022-06-20 17:29:58 -0300

    Curso concluido

---
+++ VueCompleto/09-projeto-final/35vuejs-servidor/ranek/src/views/Login.vue
@@ -0,0 +1,96 @@
+<template>
+  <section class="login">
+    <h1>Login</h1>
+    <form>
+      <label for="email">Email</label>
+      <input type="email" name="email" id="email" v-model="login.email" />
+      <label for="senha">Senha</label>
+      <input type="password" name="senha" id="senha" v-model="login.senha" />
+      <button class="btn" @click.prevent="logar">Logar</button>
+      <ErroNotificacao :erros="erros"></ErroNotificacao>
+    </form>
+    <p class="perdeu">
+      <a
+        href="http://ranekapilocal.local/wp-login.php?action=lostpassword"
+        target="_blank"
+        >Perdeu a senha? Clique aqui.</a
+      >
+    </p>
+    <LoginCriar />
+  </section>
+</template>
+
+<script>
+import LoginCriar from "@/components/LoginCriar.vue";
+import ErroNotificacao from "@/components/ErroNotificacao.vue";
+
+export default {
+  name: "LoginView",
+  components: {
+    LoginCriar,
+    ErroNotificacao,
+  },
+  data() {
+    return {
+      login: {
+        email: "",
+        senha: "",
+      },
+      erros: [],
+    };
+  },
+  methods: {
+    logar() {
+      this.erros = [];
+      this.$store
+        .dispatch("logarUsuario", this.login)
+        .then((response) => {
+          this.$store.dispatch("getUsuario", response);
+          this.$router.push({ name: "usuario" });
+        })
+        .catch((erro) => {
+          this.erros.push(erro.response.data.message);
+        });
+    },
+  },
+  created() {
+    document.title = "Ranek";
+  },
+};
+</script>
+
+<style scoped>
+.login {
+  max-width: 500px;
+  margin: 0 auto;
+  padding: 0 20px;
+}
+
+h1 {
+  text-align: center;
+  font-size: 2rem;
+  margin-top: 40px;
+  color: #87f;
+}
+
+form {
+  display: grid;
+}
+
+.btn {
+  width: 100%;
+  max-width: 300px;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+.perdeu {
+  text-align: center;
+  margin: 20px auto 0 auto;
+}
+
+.perdeu a:hover {
+  color: #87f;
+  text-decoration: underline;
+}
+</style>
---
+++ VueCompleto/09-projeto-final/35vuejs-servidor/ranek/src/views/PaginaNaoEncontrada.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>Pagina não encontrada.</h1>
+    <router-link to="/">Voltar Para Home</router-link>
+    <img src="https://http.cat/404" alt="gato" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "paginaNaoEncontrada",
+};
+</script>
+
+<style scoped>
+div {
+  margin: 20px auto;
+  text-align: center;
+}
+img {
+  margin-left: 20%;
+}
+@media screen and (max-width: 500px) {
+  img {
+    margin: 0;
+  }
+}
+</style>
